3d307bf16ef734515dac735cd562ec5162f1be5d
Rudy Gasson <2715405+rudygasson@users.noreply.github.com>
2021-10-23 15:05:21 +0200

    Show popup with basic info and zoom-in

--- spa/js/connection.js
+++ spa/js/connection.js
@@ -1,11 +1,13 @@
 import { ukToWorld } from "./grid";
 import { assert } from "./utils";
 import { merge } from '@mapbox/geojson-merge';
+import { showDetails } from "./display";
 
-const url = new URL("http://localhost:5000/earthworks");
-const areaLayers = {}
+export const BASE_URL = "http://localhost:5000/earthworks";
+const areaLayers = {};
 
 export async function getAreaCollection() {
+    let url = new URL(BASE_URL);
     return fetch(url + "/areas?q=table")
         .then(res => res.json());
 }
@@ -13,11 +15,24 @@ export async function getAreaCollection() {
 // Get geoJson-Layer for an area
 export async function getAreaLayer(area) {
     if (areaLayers[area]) return areaLayers[area];
-    let dueData = await getGeoData({next_pi: "due", area});
-    let overdueData = await getGeoData({area, next_pi: "overdue"});
+    let dueData = await getGeoData({ next_pi: "due", area });
+    let overdueData = await getGeoData({ next_pi: "overdue", area });
     let allData = merge([...dueData.features, ...overdueData.features]);
-    areaLayers[area] = L.geoJson(allData, { coordsToLatLng: ukToWorld });
+    areaLayers[area] = L.geoJson(allData, {
+        coordsToLatLng: ukToWorld,
+        style: {
+            color: 'darkorange',
+            weight: 10
+        }
+    });
+    areaLayers[area].bindPopup("Loading...");
+    areaLayers[area].on('click', showDetails);
     return areaLayers[area];
+}
+
+export async function getDetails(earthwork_number) {
+    let url = new URL(BASE_URL);
+    return fetch(url + `/${earthwork_number}`).then(res => res.json())
 }
 
 export async function getGeoData(
@@ -25,6 +40,7 @@ export async function getGeoData(
         "next_pi": "due",
         "area": 10
     }) {
+    let url = new URL(BASE_URL);
     assert(typeof url === "object", url);
     assert(url.pathname === "/earthworks", url);
     Object.keys(conditions).forEach(key => {
--- spa/js/display.js
+++ spa/js/display.js
@@ -1,17 +1,22 @@
 import "leaflet";
-import { ukToWorld, latLngToENString } from "./grid";
-import { allConditions } from "./filter";
-import { assert } from "./utils";
-
+import { latLngToENString } from "./grid";
+import { getDetails } from "./connection";
 
 const OSM_TILE_LAYER = "http://{s}.tile.osm.org/{z}/{x}/{y}.png";
 const OSM_ATTRIBUTION = `© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`;
 const ATTRIBUTION = ` | Adam Carins & <a href="https://rudygasson.de">Rudy Gasson</a>`;
 const CENTRE_OF_ENGLAND = { lat: 52.561928, lng: -1.464854 };
 const DEFAULT_ZOOM = 7;
+const DETAIL_ZOOM = 13;
+const ZOOM_DELTA = 0.25;
 
 export function initMap() {
-    const mymap = L.map('mapdiv', { center: CENTRE_OF_ENGLAND, zoom: DEFAULT_ZOOM });
+    const mymap = L.map('mapdiv', {
+        center: CENTRE_OF_ENGLAND,
+        zoom: DEFAULT_ZOOM,
+        zoomSnap: ZOOM_DELTA,
+        zoomDelta: ZOOM_DELTA
+    });
     const lyrOSM = L.tileLayer(OSM_TILE_LAYER, { attribution: OSM_ATTRIBUTION + ATTRIBUTION });
     mymap.addLayer(lyrOSM);
     return mymap;
@@ -60,11 +65,29 @@ export function createTable(areaData) {
     });
 }
 
-function description(layer) {
-    let geo = layer.feature.geometry;
-    let props = layer.feature.properties;
-    return `${props.earthwork_length_m}m ${props.earthwork_type}</br>
-    at [${geo.coordinates[0]}, ${geo.coordinates[1]}]`;
+export function showDetails(event) {
+    let map = event.target._map;
+    if (map.getZoom() < DETAIL_ZOOM) map.flyTo(event.latlng, DETAIL_ZOOM);
+    let feature = getDetails(event.layer.feature.properties.earthwork_number);
+    let popup = event.target.getPopup();
+    let view = {
+        "earthwork_number": "ID:",
+        "earthwork_type": "Type:",
+        "earthwork_length_m": "Length (m):",
+        "road": "Road:",
+        "last_complete_inspection_date": "Last Insp.:",
+        "next_principal_inspection_date": "Next Insp.:"
+    };
+    let tbody = document.createElement('tbody');
+    let td = "<td style='padding: 0 0.2rem'>"
+    feature.then(f => {
+        for (let key of Object.keys(view)) {
+            let tr = "<tr>" + td + view[key] + "</td>" + td + f.properties[key].toString() + "</td></tr>";
+            tbody.innerHTML += tr;
+        }
+        popup.setContent(tbody);
+        popup.update();
+    });
 }
 
 export { CENTRE_OF_ENGLAND, DEFAULT_ZOOM }
