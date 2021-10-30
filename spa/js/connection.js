2637fc2192694d9463b84796dfe9d681bc9db6b3
Rudy Gasson <2715405+rudygasson@users.noreply.github.com>
2021-10-31 00:14:10 +0200

    #8 Separate colors based on urgency

--- spa/js/connection.js
+++ spa/js/connection.js
@@ -1,9 +1,11 @@
 import { ukToWorld } from "./grid";
 import { assert } from "./utils";
-import { merge } from '@mapbox/geojson-merge';
 import { showDetails } from "./display";
 
 export const BASE_URL = "http://localhost:5000/earthworks";
+const DUE_COLOR = 'darkorange';
+const OVERDUE_COLOR = 'red';
+const LINE_WEIGHT = 8;
 const areaLayers = {};
 
 export async function getAreaCollection() {
@@ -12,18 +14,23 @@ export async function getAreaCollection() {
         .then(res => res.json());
 }
 
-// Get geoJson-Layer for an area
+// Get geoJSON-Layer for an area
 export async function getAreaLayer(area) {
     if (areaLayers[area]) return areaLayers[area];
     let dueData = await getGeoData({ next_pi: "due", area });
     let overdueData = await getGeoData({ next_pi: "overdue", area });
-    let allData = merge([...dueData.features, ...overdueData.features]);
-    areaLayers[area] = L.geoJson(allData, {
+    overdueData.features.map(feature => {
+        feature.properties.overdue = true;
+    });
+    areaLayers[area] = L.geoJson(dueData, {
         coordsToLatLng: ukToWorld,
         style: {
-            color: 'darkorange',
-            weight: 10
+            color: DUE_COLOR,
+            weight: LINE_WEIGHT
         }
+    });
+    areaLayers[area].addData(overdueData).eachLayer(layer => {
+        if (layer.feature.properties.overdue) layer.options.color = OVERDUE_COLOR;
     });
     areaLayers[area].bindPopup("Loading...");
     areaLayers[area].on('click', showDetails);
